8f2a10c62ae508b4e5b596bced97316d7f1d6f18
ImManiKanta <chinthamanikanta1@gmail.com>
2026-01-28 18:55:45 +0530

    shell

---
+++ datatypes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUM1=100
+NUM2=20
+
+SUM=$(($NUM1+$NUM2))
+echo "Sum is: $SUM"
+
+#string
+name="Mani"
+echo "Name is $name"
+# Array
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
